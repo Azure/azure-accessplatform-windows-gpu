7f0920afd02131c55bfd23d56566de69d5334512
Peter Longhurst <plonghurst@teradici.com>
2017-02-15 13:44:27 -0800

    Fixed registery key parameters and pointed to NVIDIA Azure specific driver

--- CustomScripts/nVIDIAdTeradiciLeostreamAgents.ps1
+++ CustomScripts/nVIDIAdTeradiciLeostreamAgents.ps1
@@ -18,29 +18,28 @@ $conName = "tdcm16sg112leo8193ls102"
 $dest = "C:\Downloadinstallers\"
 $leostreamAgentVer = $args[0]
 $teradiciAgentVer = $args[1]
-$nvidiaVer = $args[2]
+$nvidiaVer = $args[2] #Ignored - latested Azure driver is pulled from Microsoft
 $storageAcc = $args[3]
 $conName = $args[4]
 $license = $args[5]
-$registryPath = "HKLM:\Software\Teradici\PCoIP"
-$Name = "pcoip_admin"
+$registryPath = "HKLM:\Software\Teradici\PCoIP\pcoip_admin"
+$Name = "pcoip.max_encode_threads"
 $value = "8"
 $Date = Get-Date
 <#
-Write-Host "You inputs are '$leostreamAgentVer' and '$teradiciAgentVer' with '$nvidiaVer', '$storageAcc', '$conName', '$license'  on '$Date'"
+Write-Host "You inputs are '$leostreamAgentVer' and '$teradiciAgentVer' with '$storageAcc', '$conName', '$license'  on '$Date'"
 
 New-Item -Path $dest -ItemType directory
 
-wget https://$storageAcc.blob.core.windows.net/$conName/"$nvidiaVer"_grid_win10_server2016_64bit_international.exe -OutFile C:\Downloadinstallers\"$nvidiaVer"_grid_win10_server2016_64bit_international.exe
+wget https://go.microsoft.com/fwlink/?linkid=836843 -OutFile C:\Downloadinstallers\NVAzureDriver.zip
 wget http://download.opendtect.org/relman/OpendTect_Installer_win64.exe -OutFile C:\Downloadinstallers\OpendTect_Installer_win64.exe
 wget https://$storageAcc.blob.core.windows.net/$conName/PCoIP_agent_release_installer_"$teradiciAgentVer"_graphics.exe -OutFile C:\Downloadinstallers\PCoIP_agent_release_installer_"$teradiciAgentVer"_graphics.exe
 
 wget https://$storageAcc.blob.core.windows.net/$conName/LeostreamAgentSetup$leostreamAgentVer.exe -OutFile C:\Downloadinstallers\LeostreamAgentSetup$leostreamAgentVer.exe
 
-
-C:\Downloadinstallers\"$nvidiaVer"_grid_win10_server2016_64bit_international.exe /s
-Start-Sleep -s 90
-Set-Location "C:\NVIDIA\$nvidiaVer"
+New-Item -Path C:\Downloadinstallers\NVAzureDriver\ -ItemType directory
+Expand-Archive C:\Downloadinstallers\NVAzureDriver.zip -DestinationPath C:\Downloadinstallers\NVAzureDriver\
+Set-Location C:\Downloadinstallers\NVAzureDriver\
 .\setup.exe -s
 Start-Sleep -s 90
 C:\Downloadinstallers\PCoIP_agent_release_installer_"$teradiciAgentVer"_graphics.exe /S
@@ -52,33 +51,33 @@ net start nvsvc
 & 'C:\Program Files (x86)\Teradici\PCoIP Agent\licenses\appactutil.exe' appactutil.exe -served -comm soap -commServer https://teradici.flexnetoperations.com/control/trdi/ActivationService -entitlementID $license
 #>
 New-Item -Path $dest -ItemType directory
-$nvidiaUrl = [System.String]::Format("https://{0}.blob.core.windows.net/{1}/{2}_grid_win10_server2016_64bit_international.exe", $storageAcc, $conName, $nvidiaVer)
+$nvidiaMSSite = [System.String]::Format("https://docs.microsoft.com/en-us/azure/virtual-machines/virtual-machines-windows-n-series-driver-setup")
+$nvidiaUrl = [System.String]::Format("https://go.microsoft.com/fwlink/?linkid=836843")
 $teradiciAgentUrl = [System.String]::Format("https://{0}.blob.core.windows.net/{1}/PCoIP_agent_release_installer_{2}_graphics.exe", $storageAcc, $conName, $teradiciAgentVer)
 $leostreamAgentUrl = [System.String]::Format("https://{0}.blob.core.windows.net/{1}/LeostreamAgentSetup{2}.exe", $storageAcc, $conName, $leostreamAgentVer)
-$nvidiaExeName = [System.IO.Path]::GetFileName($nvidiaUrl)
+$nvidiaZipName = [System.String]::Format("{0}\NVAzureDriver.zip", $dest)
+$nvidiaTmpPath = [System.String]::Format("{0}\NVAzureDriver", $dest)
 $teradiciExeName = [System.IO.Path]::GetFileName($teradiciAgentUrl)
 $leostreamExeName = [System.IO.Path]::GetFileName($leostreamAgentUrl)
-$nvidiaExePath = [System.String]::Format("{0}{1}", $dest, $nvidiaExeName)
 $teradiciExePath = [System.String]::Format("{0}{1}", $dest, $teradiciExeName)
 $leostreamExePath = [System.String]::Format("{0}{1}", $dest, $leostreamExeName)
-Write-Host "The NVIDIA Driver exe Url  is '$nvidiaUrl'"
-Write-Host "The NVIDIA exe name is '$nvidiaExeName'"
+Write-Host "The NVIDIA Azure specific driver is from  '$nvidiaMSSite'"
+Write-Host "The NVIDIA Driver Zip file Url  is '$nvidiaUrl'"
 Write-Host "The Teradici Agent exe  Url  is '$teradiciAgentUrl'"
 Write-Host "The Teradici Agent exe name is '$teradiciExeName'"
 Write-Host "The Leostream Agent exe Url is '$leostreamAgentUrl'"
 Write-Host "The Leostream Agent exe name is '$leostreamExeName'"
-Write-Host "The NVIDIDA exe downloaded location is '$nvidiaExePath'"
 Write-Host "The Teradici Agent exe downloaded location is '$teradiciExePath'"
-Write-Host "The Leostream Agent exe downloaded location iss '$leostreamExePath'"
-wget $nvidiaUrl -OutFile $nvidiaExePath
+Write-Host "The Leostream Agent exe downloaded location is '$leostreamExePath'"
+wget $nvidiaUrl -OutFile $nvidiaZipName
 wget $teradiciAgentUrl -OutFile $teradiciExePath
 wget $leostreamAgentUrl -OutFile $leostreamExePath
 Start-Sleep -s 360
-& $nvidiaExePath  /s
-Start-Sleep -s 60
-$NVIDIAfolder = [System.String]::Format("C:\NVIDIA\{0}", $nvidiaVer)
-Write-Host "The NVIDIA Folder name is '$NVIDIAfolder'"
-Set-Location $NVIDIAfolder
+Write-Host "Expanding NVIDIA driver archive '$nvidiaZipName' to '$nvidiaTmpPath'"
+New-Item -Path $nvidiaTmpPath -ItemType directory
+Expand-Archive $nvidiaZipName -DestinationPath $nvidiaTmpPath
+Set-Location $nvidiaTmpPath
+Write-Host "Installing NVIDIA Azure specific driver"
 .\setup.exe -s -noreboot -clean
 Start-Sleep -s 180
 & $teradiciExePath /S /NoPostReboot
